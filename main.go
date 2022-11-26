714e3b47728ccffea0f549fe4dc9ed070c1058f9
Genialngash <ngangadanche@gmail.com>
2022-11-26 12:03:02 +0300

    initialize receiver

--- main.go
+++ main.go
@@ -1,53 +1,25 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
+	kafkaclient "github.com/Genialngash/sasapay-mandate-microservice/kafkaClient"
+	"github.com/Genialngash/sasapay-mandate-microservice/sasapay"
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
 	kafkaConf := kafkaclient.Conf{
-		GroupId:                  "tanda-integrations",
+		GroupId:                  "sasapay-mandate",
 		ConsumerBootstrapServers: []string{"localhost:9092"},
 		ProducerBootstrapServers: []string{"localhost:9092"},
 	}
-	//LOAD env file
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-
-	// mongoConf := mongodb.Conf{
-	// 	Uri: os.Getenv("MONGO_DB_URL"),
-	// 	Db:  "payu",
-	// }
-
-	// mongoClient, err := mongodb.New(&mongoConf)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer mongoClient.Disconnect(context.Background())
-
-	// db := mongoClient.Database(mongoConf.Db)
-
-	// rlStorage := mongodb.NewRequestLogStorage(db)
-	// prlStorage := mongodb.NewPendingRequestLogStorage(db)
-	eventSender := kafka.NewSender(&kafkaConf)
-
-	
-
-	ioReqsHander := payukafka.NewIoPaymentRequesthandler(eventSender, rlStorage, prlStorage, payuRest)
 
 	ioTopics := []string{
 
-		"IO-01-PAYU-TopupFix-DSTV-IoPaymentRequest-v1",
-		"IO-01-PAYU-TopupFix-GOTV-IoPaymentRequest-v1",
+		"sasapay-mandate-topic",
 	}
-	ioPaymentResRecvr := kafka.NewReceiver(&kafkaConf, ioTopics, ioReqsHander.Handle)
+	ioPaymentResRecvr := kafkaclient.NewReceiver(&kafkaConf, ioTopics, sasapay.MandateRequest)
 
 	//start receiver
 	ioPaymentResRecvr.ReceiveAndHandle()
